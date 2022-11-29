85c5a16a7db610a2a432fd8a16547bd454e7acf8
Soveliy <nikita.elagin2012@yandex.ru>
2022-11-29 09:21:16 +0300

    New Works

---
+++ src/assets/scripts/script.js
@@ -0,0 +1,24 @@
+// script.js
+
+// Script
+
+// Plugins
+@@include('./plugins/lazysizes.min.js');
+@@include('./plugins/fslightbox.js');
+@@include('./plugins/swiper-bundle.min.js');
+
+// Blocks
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    @@include('./blocks/tabs.js');
+    @@include('./blocks/bricks.js');
+    @@include('./blocks/page.js');
+    @@include('./blocks/popup.js');
+    @@include('./blocks/unit-block.js');
+    @@include('./blocks/text.js');
+    @@include('./blocks/slider.js');
+
+
+
+});
